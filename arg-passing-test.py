00ffc0fcca93b95e8c6c67f83a524d67e7824156
Ion Cucos <cucosion@gmail.com>
2019-02-03 16:56:24 +0200

    python examples

---
+++ arg-passing-test.py
@@ -0,0 +1,10 @@
+def func1(a1, a2):
+    print("func1: " + str(a1)+ "," + str(a2))
+def func2(a1, a2, a3="hello", a4=42):
+    print("func2: " + str(a1)+ "," + str(a2)+ "," + str(a3)+ "," + str(a4))
+def func3(a1, a2):
+    print("func3: " + str(a1)+ "," + str(a2))
+def func4(a1, a2):
+    print("func4: " + str(a1)+ "," + str(a2))
+
+func2(1,3)
---
+++ firstexample.py
@@ -0,0 +1,9 @@
+import os
+print("hello, from python")
+
+a=1
+b=2
+print(a+b)
+print(os.getenv("os"))
+print(os.getlogin())
+print(os.getcwd())
